edcb76516ea0ab54e83b74172120ce21d1d6967b
nelsonFAlmeida <a22304590@alunos.ulht.pt>
2026-05-26 12:46:37 +0100

    eee

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 resource "null_resource" "exemplo" {
   provisioner "local-exec" {
-    command = "echo 'Estrutura Terraform pronta yeet'"
+    command = "echo 'Estrutura Terraform pronta eee'"
   }
 }
