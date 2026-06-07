6f3832bb1ce1a90001c3c847c8033fe99dace827
nelson-almeida-22304590 <a22304590@alunos.ulht.pt>
2026-06-07 21:18:42 +0100

    try5

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -20,6 +20,7 @@ resource "aws_instance" "app_server" {
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   key_name                    = "finalproject-ec2-key"
   associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
 
   root_block_device {
     volume_size = 20
--- terraform/security.tf
+++ terraform/security.tf
@@ -47,3 +47,30 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_iam_role" "ec2_role" {
+  name = "finalproject-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_full_access" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "finalproject-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
